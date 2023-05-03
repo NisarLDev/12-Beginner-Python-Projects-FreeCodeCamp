f7e1a73d85929033c0b6728f8bda914f8c665c55
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-03 13:46:54 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -5,3 +5,15 @@ def get_valid_words(words):
       
     return word
   
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word) # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set() # what the user has guessed
+    
+    # getting user input
+    user_letter = input('Guess a letter: ').upper(()
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
