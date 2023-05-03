745f7ed72478d6a71736b2c30d09d75a61fe3280
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-03 13:41:05 +0200

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,1 +1,7 @@
-
+def get_valid_words(words):
+    word = random.choice(words) # randomly chooses something from the list
+    while '-' in word or ' ' in word:
+      word = random.choice(words)
+      
+    return word
+  
