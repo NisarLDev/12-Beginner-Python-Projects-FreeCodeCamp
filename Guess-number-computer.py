dabd15703510ec16a78ee85732b809b8b1a3a378
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-03 12:48:08 +0200

    Update Guess-number-computer.py

--- Guess-number-computer.py
+++ Guess-number-computer.py
@@ -6,7 +6,7 @@ def guess(x):
     while guess != random_number:
         guess = int(input(f'Guess a number between 1 and {x}: '))
         if guess < random_number:
-            print('Sorry,guess again. Too low.')
+            print('Sorry, guess again. Too low.')
         elif guess > random_number:
             print('Sorry, guess again. Too high.')
     print('Yay, congrats. You have guessed the number {random_number} correctly!!')
