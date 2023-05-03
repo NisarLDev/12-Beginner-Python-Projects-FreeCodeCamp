c13383b5bcbb5cf4ab88cf7c945deb1992fd95ce
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-03 12:44:16 +0200

    Update Guess-number-user.py

--- Guess-number-user.py
+++ Guess-number-user.py
@@ -1,17 +1,5 @@
 import random
 
-def guess(x):
-    random_number=random.randint(1, x)
-    guess = 0
-    while guess!= random_number:
-        guess = input(f'Guess a number between 1 and {x}: ')
-        if guess < random_number:
-            print('Sorry,guess again. Too low.')
-        elif guess > random_number:
-            print('Sorry, guess again. Too high.')
-    print('Yay, congrats. You have guessed the number {random_number} correctly!!')
-guess(10)
-
 def computer_guess(x):
     low = 1
     high = x
