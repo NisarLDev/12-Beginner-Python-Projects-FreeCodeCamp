c33c9492f1e1e259d4120af0683cbce6b66dbb4d
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-05 21:41:23 +0200

    Create find_next_empty function.

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,6 +1,13 @@
 def find_next_empty(puzzle):
     # finds
     
+    for r in range(9):
+        for c in range(9):
+            if puzzle[r][c] == -1:
+                return r, c
+        
+    return None, None
+    
 def solve_sudoku(puzzle):
     # solve sudoku using backtracking
     
