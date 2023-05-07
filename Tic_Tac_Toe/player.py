b0a4595e8a90bca296fec64ef1c027e4c3ceb112
Nisar Laftissi <107173728+NisarLDev@users.noreply.github.com>
2023-05-07 13:24:33 +0200

    Update player.py

--- Tic_Tac_Toe/player.py
+++ Tic_Tac_Toe/player.py
@@ -2,8 +2,13 @@ import math
 import random
 
 class Player:
-  def __init__(self, letter):
+    def __init__(self, letter):
       # letter is x or o
       self.letter = letter
   
-  # we want all players to get their next move a game
+    # we want all players to get their next move a game
+    def get_move(self, game):
+      pass
+   
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
